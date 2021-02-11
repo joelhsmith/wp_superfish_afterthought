32637c75d52a15ee5d7bf60a6a3ae2f947eff79a
Joel <joelhsmith@users.noreply.github.com>
2021-02-11 09:58:42 -0500

    Update wp_superfish_afterthought.js

--- wp_superfish_afterthought.js
+++ wp_superfish_afterthought.js
@@ -1,9 +1,9 @@
 /**
   * MINIMALLY INVASIVE SUPERFISH MENU ADD-ON (as an afterthought).
   * 
-  * This code will add the accessibile-enough Superfish menu. It is compatible with 95%+ of all WordPress Themes (that don’t come with an accessible dropdown already).   
+  * This code will add the accessibile-enough Superfish menu. It is compatible with a lot of all WordPress Themes (that do not come with an accessible dropdown already).   
   * 
-  * The Plugin’s Javascript part requires no user configuration and no custom field. It does some fancy selector detection for that.  Since each theme’s CSS is less predictable, that will small part be on a site owner to add to the “Simple Custom CSS” field. Not related to this Plugin.
+  * The Plugins Javascript part requires no user configuration and no custom field. It does some fancy selector detection for that.  Since each theme’s CSS is less predictable, that will small part be on a site owner to add to the “Simple Custom CSS” field. Not related to this Plugin.
   *
   * The software/code is provided "As is", without warranty of any kind, express or implied, including but not limited to the warranties of merchantability, 
   * fitness for a particular purpose and noninfringement. In no event shall the authors or copyright holders be liable for any claim, 
@@ -20,29 +20,21 @@ jQuery(window).load(function($) {
      * which is what superfish() instantiates on.
      */
 
-      /**
-       * FIND MENUS WITH DROPDOWNS
-       */ 
-      // Iterate over all top level menu items to detect if there is a drop down... exluding double nested ones.
-      jQuery('.menu-item-has-children').not('.menu-item-has-children > ul > .menu-item-has-children').parent().each(function(i) {
-        $menuWrapper = jQuery(this);
-        // Check if it's dropdown is hidden AND ensure superfish is not already on the menu
-        if ( $menuWrapper.find('ul:hidden').length && ! $menuWrapper.hasClass('sf-js-enabled')) {
-          $menuWrapper.superfish();	
-        }
-        
-        /**
-         * prepend with a help tip 
-         */ 
-        var SR_NOTE = "Screen readers use Tab key to navigate the following menu."
-        // since we can't know what classes are availible we inline style screen reader only text
-        var SR_ONLY = "position:absolute;left:-10000px;top:auto;width:1px;height:1px;overflow:hidden;"
-        // place it immediatly before each menu that it applies to
-        $menuWrapper.before(jQuery('<div/>', {
+    /**
+     * FIND MENUS WITH DROPDOWNS
+     */ 
+    // Iterate over all top level menu items to detect if there is a drop down... exluding double nested ones.
+    jQuery('.menu-item-has-children').not('.menu-item-has-children > ul .menu-item-has-children').parent().each(function(i) {
+      $menuWrapper =  jQuery(this);
+      // init Superfish
+      $menuWrapper.superfish();
+      var SR_NOTE = "Screen readers may use Tab key to activate this menus drop down navigation"
+      var SR_ONLY = "position:absolute;left:-10000px;top:auto;width:1px;height:1px;overflow:hidden;"
+      $menuWrapper.parent().first().before(jQuery('<div/>', {
           style: SR_ONLY,
           tabindex: '0',
           text:  SR_NOTE,
-        }));
-      });	
+      }));
+    }); 
 }( jQuery ));
 
